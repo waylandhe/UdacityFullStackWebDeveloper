4761c8dcfbf5c76f063b604f27a004c035b64650
wayland-aditazz <wayland.he@aditazz.com>
2015-03-10 11:56:18 -0700

    finished program that teaches about functions

---
+++ ProgrammingFoundationsWithPython/01_TakeABreak.py
@@ -0,0 +1,11 @@
+import time
+import webbrowser
+
+totalBreaks = 3
+breaksTaken = 0
+
+print
+while(timesPlayed < totalbreaks):
+	webbrowser.open("https://www.youtube.com/watch?v=rMnasxSxfl8")
+	time.sleep(5)
+	breaksTaken += 1
