20d61491e24a8c3a1539bc7c1d5d64869975091d
wayland-aditazz <wayland.he@aditazz.com>
2015-03-13 08:15:19 -0700

    finished example demonstarting inheritance

---
+++ ProgrammingFoundationsWithPython/07_Inheritance/inheritance.py
@@ -0,0 +1,7 @@
+class Parent():
+	def __init__(self, last_name, eye_color):
+		print("Parent Constructor Called")
+		self.last_name = last_name
+		self.eye_color = eye_color
+
+billy_cyrus = Parent("Cyrus", "blue")
