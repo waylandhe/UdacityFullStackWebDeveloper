c9dd495c7f0d977497034ff089f940cf0f9f2528
wayland-aditazz <wayland.he@aditazz.com>
2015-03-13 08:15:59 -0700

    improved comments for MovieWebsite

--- ProgrammingFoundationsWithPython/06_MovieWebsite/entertainmentCenter.py
+++ ProgrammingFoundationsWithPython/06_MovieWebsite/entertainmentCenter.py
@@ -1,7 +1,8 @@
+# This program creates instances movies I enjoyed watching and displays it on an dynamically generated webpage
 import media
 import fresh_tomatoes
 
-
+# List of movies I enjoy
 big_hero_6 = media.Movie("Big Hero 6", "A story of a young robotics prodigy named Hiro Hamada who forms a superhero team to combat a masked villain", "http://upload.wikimedia.org/wikipedia/en/4/4b/Big_Hero_6_%28film%29_poster.jpg", "https://www.youtube.com/watch?v=rD5OA6sQ97M")
 serenity = media.Movie("Serenity", "Set in 2517, Serenity is the story of the captain and crew of Serenity, a \"Firefly-class\" spaceship. The captain and first mate are veterans of the Unification War, having fought on the losing side. Their lives of petty crime are interrupted by a psychic passenger who harbors a dangerous secret.", "http://upload.wikimedia.org/wikipedia/en/9/9e/Serenity_One_Sheet.jpg", "https://www.youtube.com/watch?v=JY3u7bB7dZk")
 pokemon_the_first_movie = media.Movie("Pokemon: The First Movie", "A three part film about Pikachu's vacation, Mewtwo's origin, and Mewtwo striking back", "http://upload.wikimedia.org/wikipedia/en/f/f2/Pok%C3%A9mon_The_First_Movie.jpg", "https://www.youtube.com/watch?v=iJjaJjJLfXE")
@@ -9,8 +10,8 @@ wreck_it_ralph = media.Movie("Wreck It Ralph", "The film tells the story of the 
 ip_man = media.Movie("Ip Man", "he film focuses on events in Ip's life that supposedly took place in the city of Foshan during the Sino-Japanese War.", "http://upload.wikimedia.org/wikipedia/en/2/2f/Ipmanposter02.jpg", "https://www.youtube.com/watch?v=1AJxXQ7xojE")
 shanghai_noon = media.Movie("Shanghai Noon", "A Chinese man who travels to the Wild West to rescue a kidnapped princess. After teaming up with a train robber, the unlikely duo takes on a Chinese traitor and his corrupt boss.", "http://upload.wikimedia.org/wikipedia/en/e/ee/ShanghaiNoon_Poster.jpg", "https://www.youtube.com/watch?v=yE9VSEgV90Y")
 
+# Creating a list so I can pass it into fresh_tomatoes that will generate the dynamic page for me
 movies = [big_hero_6, serenity, pokemon_the_first_movie, wreck_it_ralph, ip_man, shanghai_noon]
 
-# fresh_tomatoes.open_movies_page(movies)
-print("module: " + big_hero_6.__module__)
-print("name: " + media.Movie.__name__)
+# Generate the HTML page in the same folder
+fresh_tomatoes.open_movies_page(movies)
--- ProgrammingFoundationsWithPython/06_MovieWebsite/media.py
+++ ProgrammingFoundationsWithPython/06_MovieWebsite/media.py
@@ -1,13 +1,16 @@
 import webbrowser
 
 class Movie():
+	""" A class for storing information relevant to movies """
 	valid_ratings = ["G", "PG", "PG-13", "R"]
+
 	def __init__(self, movie_title, movie_storyline, poster_image_url, trailer_youtube_url ):
 		self.title = movie_title
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image_url
 		self.trailer_youtube_url = trailer_youtube_url
 
+	#opens a web browser and shows relevant trailer to the movie
 	def show_trailer(self):
 		webbrowser.open(self.trailer_youtube_url)
 
